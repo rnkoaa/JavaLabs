de91bf4a35529d9280a3c7274ed64f51e510396a
rnkoaa <richard.agyei@gmail.com>
2015-04-22 05:55:32 -0500

    begining study and development of a new annotation processor

---
+++ WeProcessAnnotations/src/main/java/com/richard/annotations/MainActivity.java
@@ -0,0 +1,14 @@
+package com.richard.annotations;
+
+/**
+ * Created by rnkoaa on 4/22/15.
+ */
+public class MainActivity {
+    public static void main(String[] args) {
+        new MainActivity().run(args);
+    }
+
+    private void run(String[] args) {
+        System.out.println("runnining from main ");
+    }
+}
