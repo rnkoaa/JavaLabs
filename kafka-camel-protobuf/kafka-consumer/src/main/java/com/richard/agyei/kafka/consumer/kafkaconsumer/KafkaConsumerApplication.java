a6d0c20e12aacfe8102b936647d3d432ebd1a987
rnkoaa <richard.agyei@gmail.com>
2017-08-10 20:59:51 -0500

    protobuf serialization in kafka

--- kafka-camel-protobuf/kafka-consumer/src/main/java/com/richard/agyei/kafka/consumer/kafkaconsumer/KafkaConsumerApplication.java
+++ kafka-camel-protobuf/kafka-consumer/src/main/java/com/richard/agyei/kafka/consumer/kafkaconsumer/KafkaConsumerApplication.java
@@ -1,9 +1,14 @@
 package com.richard.agyei.kafka.consumer.kafkaconsumer;
 
+import org.amoako.agyei.kafka.serializer.KeycloakRegistrationEventDeserializer;
+import org.amoako.agyei.keycloak.RegistrationEvents;
+import org.amoako.agyei.keycloak.RegistrationEvents.KeycloakRegistrationEvent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.KafkaComponent;
+import org.apache.camel.model.dataformat.ProtobufDataFormat;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,28 +16,6 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class KafkaConsumerApplication {
-
-    @Bean
-    CamelContextConfiguration camelContextConfiguration() {
-        return new CamelContextConfiguration() {
-            @Override
-            public void beforeApplicationStart(CamelContext camelContext) {
-                // setup kafka component with the brokers
-                KafkaComponent kafka = new KafkaComponent();
-                kafka.setBrokers("{{kafka.brokers.host}}:{{kafka.brokers.port}}");
-                kafka.getConfiguration().setAutoOffsetReset("earliest");
-                kafka.getConfiguration().setGroupId("test-group-1");
-                kafka.getConfiguration().setConsumersCount(1);
-                camelContext.addComponent("kafka", kafka);
-            }
-
-            @Override
-            public void afterApplicationStart(CamelContext camelContext) {
-
-            }
-        };
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaConsumerApplication.class, args);
@@ -43,8 +26,15 @@ public class KafkaConsumerApplication {
 class ConsumerRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+        KafkaComponent kafka = getContext().getComponent("kafka", KafkaComponent.class);
+        kafka.setBrokers("{{kafka.brokers.host}}:{{kafka.brokers.port}}");
+        kafka.getConfiguration().setGroupId("in-memory-2");
+        kafka.getConfiguration().setAutoOffsetReset("earliest");
+        kafka.getConfiguration().setConsumersCount(1);
+        kafka.getConfiguration().setValueDeserializer(KeycloakRegistrationEventDeserializer.class.getName());
+
         from("kafka:{{application.kafka.topic}}")
-                .log("${body}");
-                //.to("stream:out");
+                .log("${body}")
+                .to("stream:out");
     }
 };
--- kafka-camel-protobuf/kafka-producer/src/main/java/com/richard/agyei/kafka/producer/kafkaproducer/KafkaProducerApplication.java
+++ kafka-camel-protobuf/kafka-producer/src/main/java/com/richard/agyei/kafka/producer/kafkaproducer/KafkaProducerApplication.java
@@ -1,23 +1,28 @@
 package com.richard.agyei.kafka.producer.kafkaproducer;
 
+import org.amoako.agyei.kafka.serializer.KeycloakRegistrationEventSerializer;
+import org.amoako.agyei.keycloak.RegistrationEvents;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.kafka.KafkaComponent;
+import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class KafkaProducerApplication {
-
     /*
      *    if (inputStream == null) {
      throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
      }*/
 
-     /**
+    /**
      * @param args
      */
     public static void main(String[] args) {
@@ -28,13 +33,16 @@ public class KafkaProducerApplication {
     public CommandLineRunner commandLineRunner(CamelContext camelContext) {
         return args -> {
             ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
-            //Endpoint endpoint = camelContext.getEndpoint("kafka:{{application.kafka.topic}}");
-            // producerTemplate.set
-            // producerTemplate.setDefaultEndpoint(endpoint);
-            for (int i = 0; i < 25; i++) {
-                System.out.println("Sending Message Index: " + i);
-                producerTemplate.sendBody("kafka:{{application.kafka.topic}}", "This is a message from the message route! Index. " + i);
-            }
+
+            RegistrationEvents.KeycloakRegistrationEvent registrationEvent =
+                    RegistrationEvents.KeycloakRegistrationEvent.newBuilder()
+                            .setEventTime(System.currentTimeMillis())
+                            .setUserId(UUID.randomUUID().toString())
+                            .setEmail("richard.agyei@gmail.com")
+                            .setUsername("rnkoaa")
+                            .build();
+
+            producerTemplate.sendBody("kafka:{{application.kafka.topic}}", registrationEvent);
         };
     }
 
@@ -43,11 +51,9 @@ public class KafkaProducerApplication {
         return new CamelContextConfiguration() {
             @Override
             public void beforeApplicationStart(CamelContext camelContext) {
-                // setup kafka component with the brokers
-                KafkaComponent kafka = new KafkaComponent();
+                KafkaComponent kafka = camelContext.getComponent("kafka", KafkaComponent.class);
                 kafka.setBrokers("{{kafka.brokers.host}}:{{kafka.brokers.port}}");
-                camelContext.addComponent("kafka", kafka);
-
+                kafka.getConfiguration().setSerializerClass(KeycloakRegistrationEventSerializer.class.getName());
             }
 
             @Override
